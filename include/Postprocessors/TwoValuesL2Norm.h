2d82eb5c900044e245563c3281b861c0e65793a7
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 19:42:12 +0200

    Update TwoValuesL2Norm.h

--- include/Postprocessors/TwoValuesL2Norm.h
+++ include/Postprocessors/TwoValuesL2Norm.h
@@ -1,12 +1,14 @@
-#pragma once
+#pragma once // Ensures a header file is included only once during compilation
 
-#include "ElementIntegralVariablePostprocessor.h"
+#include "ElementIntegralVariablePostprocessor.h" // The base class of our object
 
+// Declares TwoValuesL2Norm as a public subclass of ElementIntegralVariablePostprocessor
 class TwoValuesL2Norm : public ElementIntegralVariablePostprocessor
 {
 public:
-  static InputParameters validParams(); 
-  TwoValuesL2Norm(const InputParameters & parameters);
+  // Required MOOSE methods
+  static InputParameters validParams(); // Defines the input file syntax
+  TwoValuesL2Norm(const InputParameters & parameters); // Defines the constructor
 
   virtual Real getValue() const override; // Function overridden in the src file
 
