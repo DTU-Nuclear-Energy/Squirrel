8ddb6e7ec9c320504001717d24a766c375cae8db
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 19:39:45 +0200

    Update TemperatureFeedbackInt.h

--- include/Postprocessors/TemperatureFeedbackInt.h
+++ include/Postprocessors/TemperatureFeedbackInt.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include "ElementIntegralVariablePostprocessor.h"
+#include "ElementIntegralVariablePostprocessor.h" // The base class of our object
 
+// Declares TemperatureFeedbackInt as a public subclass of ElementIntegralVariablePostprocessor
 class TemperatureFeedbackInt : public ElementIntegralVariablePostprocessor
 {
 public:
+  // Required MOOSE methods
   static InputParameters validParams();
   TemperatureFeedbackInt(const InputParameters & parameters);
 
-  virtual Real getValue() const override;
+  virtual Real getValue() const override; // Function overridden in the src file
 
 protected:
-  virtual Real computeQpIntegral() override;
+  virtual Real computeQpIntegral() override; // Function overridden in the src file
   
   const VariableValue & _T_ref; // Reference temperature Quadrature-point values
   const VariableValue & _flux; // Adjoint neutron flux shape Quadrature-point values
