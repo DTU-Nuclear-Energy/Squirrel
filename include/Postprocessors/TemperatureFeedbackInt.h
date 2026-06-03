f22b0157a507b73f987921fbaa9b11d389077e31
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 19:40:41 +0200

    Update TemperatureFeedbackInt.h

--- include/Postprocessors/TemperatureFeedbackInt.h
+++ include/Postprocessors/TemperatureFeedbackInt.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once // Ensures a header file is included only once during compilation
 
 #include "ElementIntegralVariablePostprocessor.h" // The base class of our object
 
@@ -7,8 +7,8 @@ class TemperatureFeedbackInt : public ElementIntegralVariablePostprocessor
 {
 public:
   // Required MOOSE methods
-  static InputParameters validParams();
-  TemperatureFeedbackInt(const InputParameters & parameters);
+  static InputParameters validParams(); // Defines the input file syntax
+  TemperatureFeedbackInt(const InputParameters & parameters); // Defines the constructor
 
   virtual Real getValue() const override; // Function overridden in the src file
 
