b6f57e1cc740ced784694aededa0c464883bbff1
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 17:30:16 +0200

    Update WeightDNPPostprocessor.h

--- include/Postprocessors/WeightDNPPostprocessor.h
+++ include/Postprocessors/WeightDNPPostprocessor.h
@@ -13,8 +13,7 @@ public:
 protected:
   virtual Real computeQpIntegral() override;
 
-  /// The variable to compare to
-  const VariableValue & _other_var;
+  const VariableValue & _other_var; // Adjoint neutron flux shape Quadrature-point values
   const PostprocessorValue & _norm; // Normalisation factor computed by another postprocessor
   Real _lambda; // Decay constant value
 };
