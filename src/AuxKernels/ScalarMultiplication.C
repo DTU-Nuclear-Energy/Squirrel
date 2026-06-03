e6679d30201a378d15759d2d10746a8bf0940966
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 19:46:07 +0200

    Update ScalarMultiplication.C

--- src/AuxKernels/ScalarMultiplication.C
+++ src/AuxKernels/ScalarMultiplication.C
@@ -12,7 +12,6 @@ ScalarMultiplication::validParams()
   params.addRequiredCoupledVar("source_variable", "The variable to be multiplied");
   params.addRequiredParam<VariableName>("factor", "Name of the factor");
   params.addParam<Real>("normal_factor", 1.0, "The normalization factor");
-  params.addParam<unsigned int>("component", 0, "Component to output for this variable");
   return params;
 }
 
@@ -20,7 +19,6 @@ ScalarMultiplication::ScalarMultiplication(const InputParameters & parameters)
   : AuxKernel(parameters),
     _src(coupledValue("source_variable")),
     _var(_subproblem.getScalarVariable(_tid, getParam<VariableName>("factor"))),
-    _idx(getParam<unsigned int>("component")),
     _normal_factor(getParam<Real>("normal_factor"))
 {
 }
