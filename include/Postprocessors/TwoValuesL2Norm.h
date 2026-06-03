d2dc9a0b51727b3f2decedf4005aa503e74136be
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 17:24:00 +0200

    Update TwoValuesL2Norm.h

--- include/Postprocessors/TwoValuesL2Norm.h
+++ include/Postprocessors/TwoValuesL2Norm.h
@@ -6,12 +6,12 @@ class TwoValuesL2Norm : public ElementIntegralVariablePostprocessor
 {
 public:
   static InputParameters validParams();
-
   TwoValuesL2Norm(const InputParameters & parameters);
 
   virtual Real getValue() const override;
 
 protected:
   virtual Real computeQpIntegral() override;
-  const VariableValue & _other_var;
+
+  const VariableValue & _other_var; // Adjoint neutron flux shape Quadrature-point values
 };
