638689ecf8359352ab58a6966b3179cd5110c704
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 19:44:10 +0200

    Update WeightDNPPostprocessor.h

--- include/Postprocessors/WeightDNPPostprocessor.h
+++ include/Postprocessors/WeightDNPPostprocessor.h
@@ -1,17 +1,19 @@
-#pragma once
+#pragma once // Ensures a header file is included only once during compilation
 
-#include "ElementIntegralVariablePostprocessor.h"
+#include "ElementIntegralVariablePostprocessor.h" // The base class of our object
 
+// Declares WeightDNPPostprocessor as a public subclass of ElementIntegralVariablePostprocessor
 class WeightDNPPostprocessor : public ElementIntegralVariablePostprocessor
 {
 public:
-  static InputParameters validParams();
-  WeightDNPPostprocessor(const InputParameters & parameters);
+  // Required MOOSE methods
+  static InputParameters validParams(); // Defines the input file syntax
+  WeightDNPPostprocessor(const InputParameters & parameters); // Defines the constructor
 
-  virtual Real getValue() const override;
+  virtual Real getValue() const override; // Function overridden in the src file
 
 protected:
-  virtual Real computeQpIntegral() override;
+  virtual Real computeQpIntegral() override; // Function overridden in the src file
 
   const VariableValue & _other_var; // Adjoint neutron flux shape Quadrature-point values
   const PostprocessorValue & _norm; // Normalisation factor computed by another postprocessor
