de5ffc8a3f5028f50d3ebe3f6dc88da05f4e237e
NicolasGueritat <gueritat.nicolas@gmail.com>
2026-06-03 16:53:10 +0200

    Update squirrelApp.h

--- include/base/squirrelApp.h
+++ include/base/squirrelApp.h
@@ -7,18 +7,23 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#pragma once
+#pragma once // Ensures a header file is included only once during compilation
 
+/**
+ * The base class of our object: a MOOSE application. It provides all the core framework 
+ * machinery: the factory system, problem setup, execution pipeline, mesh handling, etc. 
+ * The Squirrel app just inherits all of it.
+ */
 #include "MooseApp.h"
 
+// Declares squirrelApp as a public subclass of MooseApp
 class squirrelApp : public MooseApp
 {
 public:
-  static InputParameters validParams();
-
-  squirrelApp(InputParameters parameters);
-  virtual ~squirrelApp();
-
-  static void registerApps();
-  static void registerAll(Factory & f, ActionFactory & af, Syntax & s);
+  // Required MOOSE methods
+  static InputParameters validParams(); // Defines the input file syntax
+  squirrelApp(InputParameters parameters); // Defines the constructor
+  virtual ~squirrelApp(); // MooseApp uses a virtual destructor
+  static void registerApps(); // Registers Squirrel as a MOOSE application
+  static void registerAll(Factory & f, ActionFactory & af, Syntax & s); // Connects Squirrel to the MOOSE framework 
 };
